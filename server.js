fad9b57eeaaa1fa8d5162f620b1def11336a6e6f
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-09-01 10:57:50 +0300

    delete collection

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 const server = express();
 server.use(cors());
 server.use(express.json());
-const {handleGetData,handleAddingData,handleGetCollection, handleGetUserCollection,handleDeletingData} = require('./modules/helper.js');
+const {handleGetData,handleAddingData,handleGetCollection, handleGetUserCollection,handleDeletingData,handleDeletingCollections} = require('./modules/helper.js');
 
 
 mongoose.connect(`mongodb://maiadadb:0000@cluster0-shard-00-00.ii9w9.mongodb.net:27017,cluster0-shard-00-01.ii9w9.mongodb.net:27017,cluster0-shard-00-02.ii9w9.mongodb.net:27017/myFirstData?ssl=true&replicaSet=atlas-114hrc-shard-0&authSource=admin&retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -19,6 +19,7 @@ server.post('/addmodels',handleAddingData);
 server.get('/getcollection',handleGetCollection);
 server.get('/usercollection',handleGetUserCollection);
 server.delete('/deletemodels/:modelID2',handleDeletingData)
+server.delete('/deletecollections/:collection',handleDeletingCollections);
 
 
 
